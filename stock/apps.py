7c6bf43817082e969b61bef9b94994f984d66777
Nicholas Shahan <nick.shahan@gmail.com>
2014-04-28 09:18:55 -0700

    Work in progress: stock

--- stock/apps.py
+++ stock/apps.py
@@ -3,13 +3,25 @@ from sim.operations import OperationBase, filter_by_opcode
 
 class StockLevel(OperationBase):
     """
-    A stub implementation of this operation
+    Parses stock codes and inventory levels from the provided message and sends
+    the check and commit signals.
     """
 
     @filter_by_opcode
     def handle(self, message):
+
+        # parse all stock codes and value pairs
+        parsed = parse_stock_levels(get_stock_codes, message.text)
+        stock_levels = parsed(0)
+        remaining = parsed(1)
+
+        if len(remaining) > 0:
+            # there was a parsing failure
+            message.respond("StockLevel parse failed! Successfully parsed everything before: %s" % remaining)
+            return None
+
         check_results, commit_results = self.send_signals(message=message,
-                                                          stock_levels={'a': 1, 'b': 2})
+                                                          stock_levels=stock_levels)
 
         if commit_results == None:
             message.respond("StockLevel stub failed! %s" % repr(check_results))
@@ -30,3 +42,56 @@ class StockOut(OperationBase):
             message.respond("StockOut stub failed! %s" % repr(check_results))
         else:
             message.respond("StockOut stub succeeded!")
+
+# temp location - this information will not be stored here.
+def get_stock_codes():
+    return ["A", "B", "C", "D", "AA", "BB", "CD"]
+
+
+def get_stock_codes_regex(stock_codes):
+    """
+    Produces a regex OR of all the provided stock codes
+    stock_codes: a list of stock code strings
+    returns: a regex string
+    """
+
+    regex = "("
+    for code in stock_codes:
+        regex += "|" + code
+    regex += ")"
+    return regex
+
+def parse_stock_levels(stock_codes, message):
+    re_codes = get_stock_codes_regex(stock_codes)
+    stock_levels = map()
+
+    # try to parse first stock code
+    parsed = gobble(re_codes, message)
+
+    if parsed:
+        stock_code = parsed(0)
+        parsed = gobble("\d+", parsed(1))
+        if parsed:
+            stock_inv = parsed(0)
+            stock_levels[stock_code] = int(stock_inv)
+
+    # try to parse the remaining stock codes
+    while parsed:
+        parsed = gobble(re_codes, parsed(1))
+        if parsed:
+            stock_code = parsed(0)
+            parsed = gobble("\d+", parsed(1))
+            if parsed:
+                stock_inv = parsed(0)
+                stock_levels[stock_code] = int(stock_inv)
+
+    return (stock_levels, parsed)
+
+def gobble(pattern, source):
+    matched = re.match("\\W*" + pattern, source)
+
+    if (matched):
+        matched = matched.group(0)
+        return (matched, source[len(matched):])
+
+    return None
--- stock/tests.py
+++ stock/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-
+from stock.apps import StockLevel, StockOut
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +14,40 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class SingleStockTests(TestCase):
+    """
+    All tests involving a message with a single stock code
+    """
+    def test_single_char_stock_code(self):
+        """
+        Tests the parsing of a valid message containing a single character stock code () and level
+        """
+
+        # define a test signal listener
+        def test_listener(message, **kwargs):
+            self.message = message
+            self.stock_levels = kwargs
+
+        # Create an instance of StockLevel
+        sl = StockLevel(router)
+
+        # Create a test message
+        msg = IncommingMessage(text="A1")
+
+        # Pass the test message to the handle method
+        sl.handle(msg)
+
+        # Verify the contents of the values passed with the signal
+        self.assertEqual(test_listener.message, "A1")
+        self.assertEqual(test_listener.stock_levels["A"], 1)
+
+        # codes = ["A"]
+        # message = "A1"
+        
+        # parsed = parse_stock_levels(codes, message)
+        # stock_levels = parsed(0)
+        # remaining = parsed(1)
+
+        # self.assertEqual(len(remaining), 0)
+        # self.assertEqual(stock_levels["A"], 1)
