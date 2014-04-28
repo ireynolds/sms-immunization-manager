3b1b8f1bc6259fd9f31e4c728a7a1c95a8a56ccb
Isaac Reynolds <isaacr@uw.edu>
2014-04-28 03:44:04 +0000

    Add implementation of RegexGobbler.

---
+++ operation_parser/gobbler.py
@@ -0,0 +1,83 @@
+import re
+
+DELIMS = re.compile("[\\s;,]+")
+
+def _gobble(regex, string):
+    """
+    If zero or more characters at the beginning of string match the regular
+    expression pattern, returns (a, b) where a is the corresponding matched 
+    string and b is the remainder of the string. If there is no match, a is 
+    None.
+    """
+    match = regex.match(string)
+    if match:
+        match = match.group(0)
+        remainder = string[len(match):]
+        return (match, remainder)
+    else:
+        return (None, string)
+
+def gobble(pattern, string):
+    """
+    If zero or more characters at the beginning of string match the regular
+    expression pattern, returns (a, b) where a is the corresponding matched 
+    string and b is the remainder of the string. If there is no match, a is 
+    None.
+
+    Delimiter characters at the beginning of string are stripped before 
+    applying the pattern. The delimiter characters are:
+      * any whitespace character
+      * semicolon
+      * comma
+    """
+    # Strip off leading delimiters.
+    (delim_match, delim_remainder) = _gobble(DELIMS, string)
+    if delim_match:
+        string = delim_remainder
+
+    # Look for a match on pattern after the delimiters
+    regex = re.compile(pattern)
+    return _gobble(regex, string)
+
+def gobble_all(pattern, string):
+    """
+    Applies gobble repeatedly on the given string and pattern. Returns the 
+    pair (z, b), where z is the list of all string matches and b is the 
+    remainder of the string. If there are no matches, then z is None.
+    """
+    matches = []
+    while True:
+        # Look for a match on pattern
+        match, remainder = gobble(pattern, string)
+        if not match:
+            break
+
+        # Update the current string
+        string = remainder
+        matches.append(match)
+
+    return (matches, string) if matches else (None, string)
+
+def disambiguate_o0(string):
+    """
+    Returns the given string with all instances of "o" and "O" replaced by 
+    "0". Many non-English-speaking users use "o", "O", and "0" 
+    interchangeably, and this function can be used to effectively treat all 
+    "o" and "O" characters as "0". 
+    """
+    return string.replace("o", "0").replace("O", "0")
+
+def strip_delimiters(string):
+    """
+    Returns a copy of string in which delimiter characters have been 
+    stripped from the start and end of the string.
+    """
+    # Strip off the front
+    _, string = _gobble(DELIMS, string)
+
+    # Reverse and strip off the front (back, really)
+    string = string[::-1]
+    _, string = _gobble(DELIMS, string)
+
+    # Reverse again to put in correct order and return
+    return string[::-1]
--- operation_parser/tests.py
+++ operation_parser/tests.py
@@ -7,21 +7,29 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 
-from app import OperationParser
+import app
+import gobbler
 
 class MockMessage: 
+    """
+    A useful class that satisfies the interface of a RapidSMS
+    Message on which OperationParser depends.
+    """
     def __init__(self, text):
         # Satisfy the interface upon which OperationParser.parse depends
         self.text = text
         self.fields = {}
 
-class SimpleTest(TestCase):
+class OperationParserTest(TestCase):
+    """
+    Tests for app.OperationParser.
+    """
     
     ## Helpers
-    
+
     def parse(self, text):
         # No connection -- leave it as None
-        op = OperationParser(None)
+        op = app.OperationParser(None)
         msg = MockMessage(text)
         op.parse(msg)
         return msg.fields['operations']
@@ -40,3 +48,112 @@ class SimpleTest(TestCase):
     def test_not_split_args(self):
         self.check("SL P 100",
             ("SL", "P 100"))
+
+class GobblerTest(TestCase):
+    """
+    Tests for gobbler.
+    """
+
+    ## Helpers        
+
+    def assertGobbles(self, pattern, string, exp_match, exp_remainder):
+        act_match, act_remainder = gobbler.gobble(pattern, string)
+        self.assertEqual(exp_match, act_match)
+        self.assertEqual(exp_remainder, act_remainder)
+
+    def assertGobblesAll(self, pattern, string, exp_matches, exp_remainder):
+        act_matches, act_remainder = gobbler.gobble_all(pattern, string)
+        self.assertEqual(exp_matches, act_matches)
+        self.assertEqual(exp_remainder, act_remainder)
+
+    def assertDoesNotGobble(self, pattern, string):
+        act_match, act_remainder = gobbler.gobble(pattern, string)
+        self.assertEqual(None, act_match)
+        self.assertEqual(string, act_remainder)        
+
+    def assertDoesNotGobbleAny(self, pattern, string):
+        act_matches, act_remainder = gobbler.gobble_all(pattern, string)
+        self.assertEqual(None, act_matches)
+        self.assertEqual(string, act_remainder)
+
+    ## Tests
+
+    # Test gobble
+
+    def test_gobble_no_match(self):
+        self.assertDoesNotGobble("a", "bbb")
+
+    def test_gobble_match_all(self):
+        self.assertGobbles("aa", "aa", 
+            "aa", "")
+
+    def test_gobble_match_part(self):
+        self.assertGobbles("ab", "abcd", 
+            "ab", "cd")
+
+    def test_gobble_match_one_of_repeating(self):
+        self.assertGobbles("ab", "ababab", 
+            "ab", "abab")
+
+    def test_gobble_match_with_preceding_whitespace_mix(self):
+        self.assertGobbles("aa", " \taabb", 
+            "aa", "bb")
+
+    def test_gobble_match_with_preceding_semicolons(self):
+        self.assertGobbles("aa", ";;;aabb", 
+            "aa", "bb")
+
+    def test_gobble_match_with_preceding_commas(self):
+        self.assertGobbles("aa", ",,,,aabb",
+            "aa", "bb")
+
+    def test_gobble_match_with_preceding_delimiter_mix(self):
+        self.assertGobbles("aa", ",;\t  ;,,,\taabb",
+            "aa", "bb")
+
+    def test_gobble_on_empty_string_zero_length_match(self):
+        self.assertGobbles(".*", "", 
+            "", "")
+
+    def test_gobble_leaves_leading_delimiters(self):
+        self.assertGobbles("a", "a;b",
+            "a", ";b")
+
+    # Test gobble_all
+
+    def test_gobble_all_no_matches(self):
+        self.assertDoesNotGobbleAny("aa", "bbbbbb")
+
+    def test_gobble_all_one_matches_part(self):
+        self.assertGobblesAll("aa", "aabb",
+            ["aa"], "bb")
+
+    def test_gobble_all_one_matches_all(self):
+        self.assertGobblesAll("aa", "aa",
+            ["aa"], "")
+
+    def test_gobble_all_two_matches_part(self):
+        self.assertGobblesAll("aa", "aaaabb",
+            ["aa", "aa"], "bb")
+
+    def test_gobble_all_two_matches_all(self):
+        self.assertGobblesAll("aa", "aaaaaa", 
+            ["aa", "aa", "aa"], "")
+
+    def test_gobble_all_multiple_matches_with_delimiters(self):
+        self.assertGobblesAll("aa", ";aa\t\t;,,aa;  ;;b,\tbb",
+            ["aa", "aa"], ";  ;;b,\tbb")
+
+    # Test disambiguate_o0
+
+    def test_disambiguate_o0(self):
+        self.assertEqual(
+            "0ab0q0000",
+            gobbler.disambiguate_o0("oab0qoO0O"))
+
+    # Test strip_delimiters
+
+    def test_strip_delimiters(self):
+        self.assertEqual(
+            "Hello there-",
+            gobbler.strip_delimiters("; \t,,;Hello there-   ;,;,,\t "))
