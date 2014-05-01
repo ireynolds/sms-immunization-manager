fe2309b281155c15e6a0ba0944c97358573ed309
Isaac Reynolds <isaacr@uw.edu>
2014-05-01 15:03:31 -0700

    Refactored EquipmentFailure to be exception-oriented.

--- equipment/apps.py
+++ equipment/apps.py
@@ -11,20 +11,40 @@ def _errors(results):
 def _has_errors(results):
     return len(_errors(results)) > 0
 
+class TooManyArgsException(Exception):
+    pass
+
+class UnrecognizedTextException(Exception):
+    pass
+
+def _take_at_most_one_equipment_id(args):
+    equipment_id, remaining = gobble("[A-Z]", args.upper())
+    # It's okay if equipment_id is None--this is within spec.
+
+    # Extra stuff--reject with error
+    if remaining:
+        if equipment_id:
+            error = "Message OK until %s. Provide one equipment code and nothing else. " \
+                    "Please fix and send again." % (remaining[:3],)
+            raise TooManyArgsException(error)
+        else:
+            error = "Message OK until %s. Expected an equipment code. Please fix and " \
+                    "send again." % (remaining[:3],)
+            raise UnrecognizedTextException(error)
+        message.respond(response)
+
+    return equipment_id
+
 class EquipmentFailure(OperationBase):
 
     @filter_by_opcode
     def handle(self, message):
-        equipment_id, remaining = gobble("[A-Z]", message.fields['operations']["NF"].upper())
-        # It's okay if equipment_id is None--this is within spec.
+        args = message.fields['operations']["NF"]
 
-        # Extra stuff--reject with error
-        if remaining:
-            if equipment_id:
-                response = "Message OK until %s. Provide one equipment code and nothing else. Please fix and send again." % (remaining[:3],)
-            else:
-                response = "Message OK until %s. Expected an equipment code. Please fix and send again." % (remaining[:3],)
-            message.respond(response)
+        try:
+            equipment_id = _take_at_most_one_equipment_id(args)
+        except (TooManyArgsException, UnrecognizedTextException) as e:
+            message.respond(str(e))
             return
 
         check_results, commit_results = self.send_signals(message=message,
@@ -37,3 +57,8 @@ class EquipmentFailure(OperationBase):
         else:
             message.respond("Success. Thanks for your input!")
 
+class EquipmentRepaired(OperationBase):
+
+    @filter_by_opcode
+    def handle(self, message):
+        pass
