7ed5453fa346d777aaf471588efb80ca6cdbccbb
Jenny Kang <jskang16@gmail.com>
2014-05-07 18:27:21 -0700

    finished first draft implementation of user models

--- permissions/models.py
+++ permissions/models.py
@@ -1,31 +1,31 @@
 from rapidsms.models import Contact
 from django.db import models
 
-# Create your models here.
-
 class HierarchyNode(models.Model):    
-    # The parent of this entity in the hierarchy. If None, indicates a root node
-    parent = models.ForeignKey('self', blank=True, null=True)
+    # The parent of this entity in the hierarchy. If blank, indicates a root node
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='children')
 
     # The name of the administrative level for the node in the hierarchy
     name = models.CharField(max_length=200)
 
     def __unicode__(self):
         if self.parent:
-            return "level: %s (parent level: %s )" % (self.name, self.parent.name)
+            return "%s (parent is %s )" % (self.name, self.parent.name)
         else:
-            return "root: %s" % self.name
+            return "%s is root" % self.name
 
 class Facility(models.Model):
     # The complete name of this facility
     name = models.CharField(max_length=250)
    
-    # TODO: make phone_number / address optional?
+    #TODO: remove phone_number / address ?
+    #TODO: add location?
+    #TODO: add administrator?
     # The phone number for this facility
-    phone_number = models.CharField(max_length=25)
+    phone_number = models.CharField(max_length=25, blank=True)
 
     # The address of this facility
-    address = models.CharField(max_length=200)
+    address = models.CharField(max_length=200, blank=True)
 
     # The position of this facility in the administrative hierarchy
     hierarchy_node = models.ForeignKey(HierarchyNode)
@@ -37,9 +37,6 @@ class Facility(models.Model):
     def __unicode__(self):
         return self.name
 
-#   TODO: Location
-#   TODO: Administrator
-
 class Role(models.Model):
     # The name of this role
     name = models.CharField(max_length=100)
@@ -48,7 +45,10 @@ class Role(models.Model):
     description = models.TextField(blank=True)
 
     # The opcodes for operations that this role is allowed to carry out
-    op_code = models.CharField(max_length=200)
+    op_codes = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
 
 class SimContact(Contact):
     # The role and permitted operations for this user
