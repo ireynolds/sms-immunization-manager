ed06aebf025f48922f5c102fe7b9918a059368c8
Jackson Roberts <jcwr@uw.edu>
2014-05-14 13:09:16 -0700

    Moved boilerplate web layout code from prototype to main project

--- moderation/views.py
+++ moderation/views.py
@@ -1,1 +1,32 @@
 # Create your views here.
+
+
+def home(request):
+    """
+    A landing page which redirects to a user's default facility.
+    """
+    pass
+
+def root_nodes(request):
+    """
+    Lists the root hierarchy notes
+    """
+    pass
+
+def node(request, node_id):
+    """
+    Displays a single hierarchy node
+    """
+    pass
+
+def facility(request, facility_id):
+    """
+    Displays a summary of a facility
+    """
+    pass
+
+def user(request, user_id):
+    """
+    Displays a summary of a user
+    """
+    pass
