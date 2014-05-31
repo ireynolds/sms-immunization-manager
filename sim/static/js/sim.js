d586d4d5d2eed30f7ef24e319fb7175ffd4aa3b7
Jackson Roberts <jcwr@uw.edu>
2014-05-31 16:47:59 -0700

    Added hiding/showing of message effects. Added UI for dismissal (not yet implemented. Miscellaneous UI tweaks

--- sim/static/js/sim.js
+++ sim/static/js/sim.js
@@ -14,9 +14,27 @@ $('#language-select').change(
         $(this).closest('form').trigger('submit');
     });
 
-// Activate Bootstrap's tooltips
+// Hide/show a table of message effects
+$('.message-effect-show').click(
+    function() {
+        $(this).parent().next("table").show()
+        $(this).siblings(".message-effect-hide").show()
+        $(this).hide()
+    });
+
+$('.message-effect-hide').click(
+    function() {
+        $(this).parent().next("table").hide()
+        $(this).siblings(".message-effect-show").show()
+        $(this).hide()
+    });
+
+// Activate Bootstrap's tooltips. Every element containing a tooltip must have
+// tooltip() called on it. Elements without tooltips may have tooltip() called,
+// but for efficiency we try to narrow the set of elelments that are called.
 $(document).ready(function () {
     $('.label').tooltip();
     $('.conversation-icon').tooltip();
-    $('th').tooltip();
+    $('a').tooltip();
+    $('button').tooltip();
 });
