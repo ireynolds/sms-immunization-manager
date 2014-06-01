e7c743b790d6fbfd7a925ad3d9aabd74e18bbee7
Isaac Reynolds <isaacr@uw.edu>
2014-06-01 18:23:44 +0000

    Removed defunct prototype/.

--- prototype/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('prototype.views',
-    url(r'^$', 'facility_list'),
-    url(r'^facility/(\d+)/$', 'facility'),
-    url(r'^user/(\d+)/$', 'user'),
-)
