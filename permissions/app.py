747435dca254c245f6caa2659d301dae59e3f5e9
Jenny Kang <jskang16@gmail.com>
2014-06-01 20:10:46 -0700

    saving message effects generated by spam filter

--- permissions/app.py
+++ permissions/app.py
@@ -1,6 +1,6 @@
 from rapidsms.models import Connection
 from rapidsms.apps.base import AppBase
-from rapidsms.contrib.messagelog.app import *
+from messagelog import *
 from moderation.models import * #urgent, info, complete_effect, FILTER
 from django.utils.translation import ugettext_noop as _
 
@@ -8,21 +8,18 @@ class SpamFilter(AppBase):
     def filter(self, msg):
         """Filter out messages from unrecognized users"""
         if msg.connections[0].contact is None:
-            effect = urgent(
+            effect = error(
                     _("Received message from unrecognized user"), {},
                     _("%(phone_number)s is an unrecognized user"), { 'phone_number' : msg.connections[0].identity }
                     )
-#            mlogger = MessageLogApp(None)
-#            mlogger.parse(msg)
-#            complete_effect(effect, msg.logger_msg, FILTER)
-#
+            complete_effect(effect, msg.logger_msg, FILTER)
+
             # If message is spam, halt further processing of message
             return True
         else:
-            # TODO: Save the message effects!
             effect = info(
                     _("Received message from recognized user"), {},
                     _("%(phone_number)s is a recognized user"), { 'phone_number' : msg.connections[0].identity }
                     )
-            #complete_effect(effect, msg.logger_msg, FILTER, opcode=None)
+            complete_effect(effect, msg.logger_msg, FILTER)
             return False
