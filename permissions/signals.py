bfb4d0043ad2861ba3db97952b86836546171ce0
Jenny Kang <jskang16@gmail.com>
2014-06-12 14:21:06 -0700

    added facility code check as part of permissions check

--- permissions/signals.py
+++ permissions/signals.py
@@ -18,17 +18,17 @@ def opcode_permissions_check(message, opcode, **kwargs):
                  'role' : contact_profile.get_role_name_display() }
     
     if opcode in permitted_opcodes:
-        # TODO: add facility code check as part of permissions check
-#        if 'facility' in message.field and message.fields['facility'] == message.connections[0].contact.contactprofile.facility:
-#            fmt_args['facility'] = message.fields['facility']
-#       
-#            effect = error(
-#                _("Operation not permitted for this user for this facility"), {},
-#                _("Not allowed to perform operation %(requested_opcode)s at %(facility)s"), fmt_args
-#            )
-#
-#        else:
-           effect = info(
+        # Checks to see if the user is allowed to perform this operation for the facility specified in the message
+        if 'facility' in message.fields and message.fields['facility'] != message.connections[0].contact.contactprofile.facility:
+            fmt_args['facility'] = message.fields['facility']
+       
+            effect = error(
+                _("Operation not permitted for this user for this facility"), {},
+                _("Not allowed to perform operation %(requested_opcode)s at %(facility)s"), fmt_args
+            )
+
+        else:
+            effect = info(
                 _("Operation allowed"), {},
                 _("%(role)s requested to perform permitted operation %(requested_opcode)s."), fmt_args 
             )
